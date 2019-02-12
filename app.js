6c5f3eb5ae107f81f3aa0752f4173274b66d13b0
Sarath TK <sarth.tk@eglobeits.com>
2019-02-12 15:22:41 +0530

    Add swipe event :hammer: hammerjs

--- app.js
+++ app.js
@@ -7,6 +7,41 @@ app.controller('AppController', function ($scope, $location) {
   $scope.title = 'Hello - World';
   $scope.dirClass = 'slide-left';
 
+  $scope.routes = ['/', '/about', '/examples']
+
+  $scope.currentRoute = '';
+
+  let mainEl = document.querySelector('main');
+  let mc = new Hammer(mainEl)
+
+  mc.on('swipeleft swiperight tap', (event) => {
+
+    if (event.isFinal && (event.type === 'swiperight' || event.type === 'swipeleft')) {
+
+      $scope.dirClass = event.type === 'swipeleft' ? 'slide-left' : 'slide-right';
+
+
+
+      let currentPathIndex = $scope.routes.indexOf($location.path());
+
+      if (currentPathIndex < 0 || currentPathIndex > $scope.routes.length) return;
+
+      let nextPathOIndex = 0;
+      if (event.type === 'swipeleft') {
+        nextPathOIndex = currentPathIndex + 1;
+      } else {
+        nextPathOIndex = currentPathIndex - 1;
+      }
+
+
+
+      $location.path($scope.routes[nextPathOIndex]);
+      $scope.$apply();
+
+
+    }
+  })
+
   $scope.toggleSlideDirection = function () {
 
     $scope.dirClass = ($scope.dirClass === 'slide-left') ? 'slide-right' : 'slide-left';
