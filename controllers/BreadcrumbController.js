5776d422c42a725495ee59fe53d6ba043eecc979
Sarath <sarath.hacks@gmail.com>
2019-03-21 14:41:06 +0530

    breadcrumb :ambulance:

--- controllers/BreadcrumbController.js
+++ controllers/BreadcrumbController.js
@@ -1,144 +1,164 @@
-app.controller('BreadcrumbController', function ($scope) {
+app.controller("BreadcrumbController", function($scope) {
+  $scope.products = [
+    {
+      id: 1,
+      name: "Product 1"
+    },
+    {
+      id: 2,
+      name: "Product 2"
+    },
+    {
+      id: 3,
+      name: "Product 3"
+    },
+    {
+      id: 4,
+      name: "Product 4"
+    },
+    {
+      id: 5,
+      name: "Product 5"
+    },
+    {
+      id: 6,
+      name: "Product 6"
+    },
+    {
+      id: 7,
+      name: "Product 7"
+    },
+    {
+      id: 8,
+      name: "Product 8"
+    },
+    {
+      id: 9,
+      name: "Product 9"
+    },
+    {
+      id: 10,
+      name: "Product 10"
+    }
+  ];
 
-    $scope.products = [{
-            id: 1,
-            name: "Product 1"
-        },
-        {
-            id: 2,
-            name: "Product 2"
-        },
-        {
-            id: 3,
-            name: "Product 3"
-        },
-        {
-            id: 4,
-            name: "Product 4"
-        },
-        {
-            id: 5,
-            name: "Product 5"
-        },
-        {
-            id: 6,
-            name: "Product 6"
-        },
-        {
-            id: 7,
-            name: "Product 7"
-        },
-        {
-            id: 8,
-            name: "Product 8"
-        },
-        {
-            id: 9,
-            name: "Product 9"
-        },
-        {
-            id: 10,
-            name: "Product 10"
-        }
+  $scope.perPage = 5;
 
-    ]
+  $scope.activeIndex = 0;
 
-    $scope.perPage = 5;
+  $scope.totalPages = Math.ceil($scope.products.length / $scope.perPage);
 
-    $scope.activeIndex = 0;
+  $scope.selectedProduct = $scope.products[$scope.activeIndex];
 
-    $scope.totalPages = Math.ceil($scope.products.length / $scope.perPage);
+  $scope.currentPageSegment = null;
+  $scope.showLHalf = false;
+  $scope.showRHalf = false;
 
-    $scope.selectedProduct = $scope.products[$scope.activeIndex];
+  $scope.onDotClick = function(product) {
+    // console.log('Product ', product)
+    let _activeIndex = $scope.products.findIndex(p => {
+      return p.id === product.id;
+    });
 
-    $scope.currentPageSegment = null;
+    if (_activeIndex < 0) return;
 
-    $scope.onDotClick = function (product) {
+    $scope.activeIndex = _activeIndex;
 
-        // console.log('Product ', product)
-        let _activeIndex = $scope.products.findIndex((p) => {
+    $scope.selectedProduct = product;
 
-            return p.id === product.id
+    var _t = setTimeout(function() {
+      $scope.buildCrumbs();
+      $scope.$apply();
+      clearTimeout(_t);
+    }, 200);
+  };
 
-        })
+  $scope.clickNext = function() {
+    if ($scope.activeIndex === $scope.products.length - 1) return;
+    $scope.selectedProduct = $scope.products[$scope.activeIndex + 1];
 
-        console.log('---- ', _activeIndex)
+    $scope.setAciveIndex();
 
-        if (_activeIndex < 0) return;
+    var _t = setTimeout(function() {
+      $scope.buildCrumbs();
+      $scope.$apply();
+      clearTimeout(_t);
+    }, 200);
+  };
 
-        $scope.activeIndex = _activeIndex;
+  $scope.clickPrev = function() {
+    if ($scope.activeIndex === 0) return;
+    $scope.selectedProduct = $scope.products[$scope.activeIndex - 1];
 
-        console.log('Product Index ', $scope.activeIndex)
-        $scope.selectedProduct = product;
+    $scope.setAciveIndex();
+
+    var _t = setTimeout(function() {
+      $scope.buildCrumbs();
+      $scope.$apply();
+      clearTimeout(_t);
+    }, 200);
+  };
+
+  $scope.setAciveIndex = function() {
+    //
+    $scope.activeIndex = $scope.products.findIndex(p => {
+      return p.id === $scope.selectedProduct.id;
+    });
+  };
+
+  $scope.buildCrumbs = function() {
+    if ($scope.activeIndex === 0) {
+      $scope.currentPageSegment = $scope.products.slice(0, $scope.perPage);
     }
 
-    $scope.clickNext = function () {
-
-        if ($scope.activeIndex === $scope.products.length - 1) return;
-        $scope.selectedProduct = $scope.products[$scope.activeIndex + 1]
-
-        $scope.setAciveIndex()
-
-
-    }
-
-    $scope.clickPrev = function () {
-
-        if ($scope.activeIndex === 0) return;
-        $scope.selectedProduct = $scope.products[$scope.activeIndex - 1];
-
-        $scope.setAciveIndex()
-    }
-
-    $scope.setAciveIndex = function () {
-
-        //
-        $scope.activeIndex = $scope.products.findIndex((p) => {
-
-            return p.id === $scope.selectedProduct.id
-
-        })
-    }
-
-
-    $scope.getProducts = function () {
-        console.log('>', $scope.activeIndex)
-
-        if ($scope.activeIndex === 0) {
-
-            $scope.currentPageSegment = $scope.products.slice(0, $scope.perPage);
-        }
-
-
-        let _activeProductIdx = $scope.currentPageSegment.findIndex(function (p) {
-
-            return (p.id === $scope.selectedProduct.id)
-
-        })
-        // forword ->
-        if (_activeProductIdx == 4) {
-
-            if ($scope.activeIndex === ($scope.products.length - 1)) {
-                return $scope.currentPageSegment;
-            }
-            $scope.currentPageSegment.shift();
-            $scope.currentPageSegment.push($scope.products[$scope.activeIndex + 1])
-        }
-
-        // backword <-
-        if (_activeProductIdx == 0 && $scope.activeIndex !== 0) {
-
-            if ($scope.activeIndex === 0) return $scope.currentPageSegment;
-            $scope.currentPageSegment.pop();
-            $scope.currentPageSegment.unshift($scope.products[$scope.activeIndex - 1])
-        }
-
-        $scope.setAciveIndex();
-
+    let _activeProductIdx = $scope.currentPageSegment.findIndex(function(p) {
+      return p.id === $scope.selectedProduct.id;
+    });
+    // forword ->
+    if (_activeProductIdx == $scope.perPage - 1) {
+      if ($scope.activeIndex === $scope.products.length - 1) {
         return $scope.currentPageSegment;
-
+      }
+      $scope.currentPageSegment.shift();
+      $scope.currentPageSegment.push($scope.products[$scope.activeIndex + 1]);
     }
 
+    // backword <-
+    if (_activeProductIdx == 0 && $scope.activeIndex !== 0) {
+      if ($scope.activeIndex === 0) return $scope.currentPageSegment;
+      $scope.currentPageSegment.pop();
+      $scope.currentPageSegment.unshift(
+        $scope.products[$scope.activeIndex - 1]
+      );
+    }
 
+    $scope.setAciveIndex();
 
-})
+    /////////////////////////
+    if ($scope.activeIndex == 0) {
+      console.log("+ y");
+      $scope.showLHalf = false;
+    } else {
+      $scope.showLHalf = true;
+    }
+
+    if (
+      $scope.products.length > $scope.perPage &&
+      $scope.activeIndex !== $scope.products.length - 1
+    ) {
+      console.log("*y");
+      $scope.showRHalf = true;
+    } else {
+      console.log("*np");
+      $scope.showRHalf = false;
+    }
+
+    /////////////////////////
+
+    $scope.currentPageSegment = $scope.currentPageSegment;
+
+    return $scope.currentPageSegment;
+  };
+
+  $scope.buildCrumbs();
+});
